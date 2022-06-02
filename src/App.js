9c0c4adbb067860d6a6ec7274636fd1eadc4ff2b
bkounter <bradleykounter@gmail.com>
2022-06-02 09:37:11 -0700

    Task tracker app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
-  const name = 'Brad'
-  const x = true
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 11:30am',
+        reminder: true,
+      },
+    ]
+  )
   
   return (
     <div className='container'>
-      <Header title='Custom title' />
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
